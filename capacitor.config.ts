5f59f38f6e0adfd12a3f3538e71162fa590d2871
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-09-02 12:01:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Stranger_E-Sport',
+  webDir: 'www'
+};
+
+export default config;
