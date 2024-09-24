c7fe59e7b07aba464ae6df51bd3d5fbccc073799
ariril <arilmanek88@gmail.com>
2024-09-24 17:45:40 +0700

    tes

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -25,15 +25,16 @@ import {
     NgStyle,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-
 })
 export class WelcomePage implements OnInit {
+
   onboardingScreens = [
     { image: '1.jpg' },
     { image: '2.jpg' },
     { image: '3.jpg' },
   ];
   swiperModules = [IonicSlides];
+
   constructor() { }
 
   ngOnInit() {
