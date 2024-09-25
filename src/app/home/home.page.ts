31991b860a6c4c3ad480da418bf25643bb3bde4a
anyazefanya <zefanyaana@gmail.com>
2024-09-25 17:16:53 +0700

    event di home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() { }
+  events = [
+    {
+      imgSrc: '../../assets/img/Home/thumbnail/mpl1.jpg',
+      title: 'Golden Moment Week 6 presented by UBS Gold #MPLIDS14',
+    },
+    {
+      imgSrc: '../../assets/img/Home/thumbnail/valorant.jpg',
+      title: 'EVENT HORIZON : SINGULARITY REVEAL TRAILER',
+    },
+    {
+      imgSrc: '../../assets/img/Home/thumbnail/mpl2.jpg',
+      title: 'RRQ HOSHI vs BIGETRON ALPHA | Regular Season Week 6 Day 3 | Game 3 | #MPLIDS14',
+    },
+    {
+      imgSrc: '../../assets/img/Home/thumbnail/hok.jpg',
+      title: 'Arli - Memory Chip | Skin Showcase Video',
+    },
+    {
+      imgSrc: '../../assets/img/Home/thumbnail/pubg.jpg',
+      title: 'VAMPIRE ESPORTS THE NEW KING OF SEA',
+    }
 
+  ];
 }
