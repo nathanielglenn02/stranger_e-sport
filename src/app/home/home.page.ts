28eb3253ac3e2927924dba97b5aa6eea26f454af
ariril <arilmanek88@gmail.com>
2024-09-26 12:44:49 +0700

    perbaikan home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,28 +10,28 @@ export class HomePage {
   constructor() { }
   ytbs = [
     {
-      imgSrc: 'assets/img/home/thumbnail/mpl2.jpg',
+      imgSrc: 'assets/img/Home/thumbnail/mpl2.jpg',
       title: 'RRQ HOSHI vs BIGETRON ALPHA | Regular Season Week 6 Day 3 | Game 3 | #MPLIDS14',
       url: 'https://youtu.be/GDB7E8vwKR8?feature=shared'
     },
     {
-      imgSrc: 'assets/img/home/thumbnail/pubg.jpg',
+      imgSrc: 'assets/img/Home/thumbnail/pubg.jpg',
       title: 'VAMPIRE ESPORTS THE NEW KING OF SEA',
       url: 'https://youtu.be/6nYoiH4SaBA?feature=shared'
     },
     {
-      imgSrc: 'assets/img/home/thumbnail/mpl1.jpg',
+      imgSrc: 'assets/img/Home/thumbnail/mpl1.jpg',
       title: 'Golden Moment Week 6 presented by UBS Gold #MPLIDS14',
       url: 'https://youtu.be/eQ2m_Pc3BkE?feature=shared'
     },
     {
-      imgSrc: 'assets/img/home/thumbnail/valorant.jpg',
+      imgSrc: 'assets/img/Home/thumbnail/valorant.jpg',
       title: 'EVENT HORIZON : SINGULARITY REVEAL TRAILER',
       url: 'https://youtu.be/Kwh4FtFgTCI?feature=shared'
     },
 
     {
-      imgSrc: 'assets/img/home/thumbnail/hok.jpg',
+      imgSrc: 'assets/img/Home/thumbnail/hok.jpg',
       title: 'Arli - Memory Chip | Skin Showcase Video',
       url: 'https://youtu.be/Rb6FNRaAufk?feature=shared'
     }
