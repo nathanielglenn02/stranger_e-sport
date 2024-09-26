60de36b26f4fb5873ed221690068c997086860b9
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-09-26 10:49:39 +0700

    Home selesai

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,32 +10,56 @@ export class HomePage {
   constructor() { }
   ytbs = [
     {
-      imgSrc: '../../assets/img/home/thumbnail/mpl2.jpg',
+      imgSrc: 'assets/img/home/thumbnail/mpl2.jpg',
       title: 'RRQ HOSHI vs BIGETRON ALPHA | Regular Season Week 6 Day 3 | Game 3 | #MPLIDS14',
       url: 'https://youtu.be/GDB7E8vwKR8?feature=shared'
     },
     {
-      imgSrc: '../../assets/img/home/thumbnail/pubg.jpg',
+      imgSrc: 'assets/img/home/thumbnail/pubg.jpg',
       title: 'VAMPIRE ESPORTS THE NEW KING OF SEA',
       url: 'https://youtu.be/6nYoiH4SaBA?feature=shared'
     },
     {
-      imgSrc: '../../assets/img/home/thumbnail/mpl1.jpg',
+      imgSrc: 'assets/img/home/thumbnail/mpl1.jpg',
       title: 'Golden Moment Week 6 presented by UBS Gold #MPLIDS14',
       url: 'https://youtu.be/eQ2m_Pc3BkE?feature=shared'
     },
     {
-      imgSrc: '../../assets/img/home/thumbnail/valorant.jpg',
+      imgSrc: 'assets/img/home/thumbnail/valorant.jpg',
       title: 'EVENT HORIZON : SINGULARITY REVEAL TRAILER',
       url: 'https://youtu.be/Kwh4FtFgTCI?feature=shared'
     },
 
     {
-      imgSrc: '../../assets/img/home/thumbnail/hok.jpg',
+      imgSrc: 'assets/img/home/thumbnail/hok.jpg',
       title: 'Arli - Memory Chip | Skin Showcase Video',
       url: 'https://youtu.be/Rb6FNRaAufk?feature=shared'
     }
   ];
+
+  evts = [
+    {
+      imgSrc: 'assets/img/Schedule/mpl.jpg',
+      title: 'MPL XXIV - Single Elimination',
+      url: 'https://youtu.be/GDB7E8vwKR8?feature=shared'
+    },
+    {
+      imgSrc: 'assets/img/Schedule/m6.jpg',
+      title: 'M6 MLBB - Single Elimination',
+      url: 'https://youtu.be/6nYoiH4SaBA?feature=shared'
+    },
+    {
+      imgSrc: 'assets/img/Schedule/valochamp.jpg',
+      title: 'Valorant Champions Tour (VCT) 2024 - Single Elimination',
+      url: 'https://youtu.be/eQ2m_Pc3BkE?feature=shared'
+    },
+    {
+      imgSrc: 'assets/img/Schedule/cocwc.jpg',
+      title: 'Clash of Clans World Championship 2024 - Single Elimination',
+      url: 'https://youtu.be/Kwh4FtFgTCI?feature=shared'
+    }
+  ];
+
   openLink(url: string) {
     window.open(url, '_blank');
   }
