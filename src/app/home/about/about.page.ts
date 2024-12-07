53696c7f24f5f2929636bad6f44c3240b718b659
ariril <arilmanek88@gmail.com>
2024-12-07 20:38:46 +0700

    local Storage for like

--- src/app/home/about/about.page.ts
+++ src/app/home/about/about.page.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage {
   likes: number = 0;
+  temp = ""
 
   constructor() {}
+  ngOnInit() {
+    this.temp = localStorage.getItem("project_likes") ?? "";
+    if (this.temp == "") {
+      this.likes = Number.parseInt(this.temp);
+      alert(this.temp)
+    }
+    else {
+      this.likes = 0
+    }
+  }
 
   likeTeam() {
     this.likes += 1;
+    localStorage.setItem("project_likes", this.likes.toString());
   }
 }
