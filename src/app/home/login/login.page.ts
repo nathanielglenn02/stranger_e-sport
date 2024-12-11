e10e07a3f13b66089acf1f99cae64da3d03650d0
ariril <arilmanek88@gmail.com>
2024-12-11 13:48:51 +0700

    delete comment

--- src/app/home/login/login.page.ts
+++ src/app/home/login/login.page.ts
@@ -35,27 +35,22 @@ export class LoginPage implements OnInit {
     this.authservice.login(this.username, this.password).subscribe(
       (response: any) => {
         if (response.result === 'OK') {
-          // Ambil data user dari array pertama di response.data
           const userData = response.data[0];
           this.fullname = `${userData.fname} ${userData.lname}`;
           this.idmember =  `${userData.idmember}`;
 
           alert(`Login successful! Welcome, ${this.fullname}`);
 
-          // Simpan ke localStorage
           localStorage.setItem("project_username", this.username);
           localStorage.setItem("project_fullname", this.fullname);
           localStorage.setItem("project_idmember", this.idmember);
 
-          // Lakukan tindakan setelah login
           this.checkLogin();
         } else {
-          // Tampilkan pesan error jika login gagal
           alert(response.message);
         }
       },
       (error) => {
-        // Tangani error dari HTTP request
         console.error('Login Error:', error);
         alert('Login failed. Please try again.');
       }
